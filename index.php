417402ed630f1f74d5215e675f808e96bb1a106b
Gustas <gustass1@gmail.com>
2020-04-05 07:34:24 +0200

    feat: updated navigation, added proper links to pages

--- index.php
+++ index.php
@@ -18,9 +18,21 @@ checkLoggedIn();
   <body>
     <nav class='flex items-center justify-end h-16 bg-blue-400 shadow-xl text-white'>
       <ul class='flex'>
-        <li class='mx-3 cursor-pointer'>Home</li>
-        <li class='mx-3 cursor-pointer'>Login</li>
-        <li class='mx-3 cursor-pointer'>Register</li>
+        <a href="index.php">
+          <li class='mx-3 cursor-pointer'>Home</li>
+        </a>
+        <a href="pages/schedule.php">
+          <li class='mx-3 cursor-pointer'>Schedule</li>
+        </a>
+        <a href='pages/user.php'><li>
+          <svg class='mx-3 h-6' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path class='fill-current' d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
+          </svg>
+        </li>
+        </a>
+        <a href="api/logout.php?token=<?= $_SESSION['user_token']?>">
+          <li class='mx-3 cursor-pointer'>Logout</li>
+        </a>
       </ul>
     </nav>
     <script src="js/main.js"></script>
