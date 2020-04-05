4aa80cde5bd4e265c1276c5ea38cb649cb8bc6c0
Gustas <gustass1@gmail.com>
2020-04-05 17:17:32 +0200

    feat: added error reporting

--- core/config.php
+++ core/config.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Error reporting; disabled
+ */
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
 define('MYSQL_USER', 'root');
 define('MYSQL_PASSWORD', '');
 define('MYSQL_HOST', 'localhost');
