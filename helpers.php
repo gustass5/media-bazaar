db9fb6557e7b65aec52620e17d53b1ecfb5be1ab
Gustas <gustass1@gmail.com>
2020-04-05 07:35:17 +0200

    feat(helpers): added functions for login check and fetching user data

---
+++ helpers.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+require 'core/config.php';
+
+
+function checkLoggedIn($redirect = true){
+    if(!isset($_SESSION['user_id']) || !isset($_SESSION['logged_in'])){
+        if($redirect){
+            header('Location: login.php');
+            exit;
+        }else{
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function fetchUserData($userId, $sql){
+    Global $pdo;
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(':user_id', $userId);
+    $statement->execute();
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $user;
+}
+
+
+?>
