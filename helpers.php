a6fde81d48d7e376ca0f643c5fb01372140d756d
Gustas <gustass1@gmail.com>
2020-04-05 17:18:14 +0200

    fix: not starting session if it is already started

--- helpers.php
+++ helpers.php
@@ -1,8 +1,10 @@
 <?php
 
-session_start();
+if(!isset($_SESSION)) { 
+    session_start(); 
+} 
 
-require 'core/config.php';
+require_once 'core/config.php';
 
 
 function checkLoggedIn($redirect = true){
